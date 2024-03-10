dcb641faf349073620678993e76e32a377942ac5
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-03-10 11:58:26 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    Product p=new Product();
+    p.fun(10);
+    p.fun(2,3);
+    p.fun(5,6,7);
+}
+public class Product{
+  //int x,y,z;
+  void fun(int x){
+    
+    System.out.println("This only one parameter method"+x);
+  }
+  void fun(int x,int y,int z){
+    System.out.println("This three parameters method "+x+" ,"+y+" ,"+z);
+  }
+  void fun(int x,int y){
+    System.out.println("This two parameters method "+x+" ,"+y);
+  }
 }
