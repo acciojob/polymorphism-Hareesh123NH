ea7a05596078adb52762879854d6bce7c9f9cfa2
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-05-06 20:46:17 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,6 @@
 package com.driver;
 
 public class Main {
-  class Product{
     public static void main(String[] args){
       Product p=new Product();
       A=p.product(10,20);
@@ -10,18 +9,4 @@ public class Main {
       C=p.product(a,b);
       System.out.println("1st method "+A+"\n2nd method "+B+"\n3rd method "+C);
     }
-    public int product(int x,int y){
-      int re=x*y;
-      return re;
-    }
-    public int product(int x,int y,int z){
-      int re=x*y*z;
-      return re;
-    }
-    public double product(double x,double y){
-      double re=x*y;
-      return re;
-    }
-  }
-  
 }
