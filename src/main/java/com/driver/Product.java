f80775d2400701ea1a7c8e69633d4f5ac71d084b
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-05-06 21:26:03 +0530

    remove Product.java

--- src/main/java/com/driver/Product.java
+++ src/main/java/com/driver/Product.java
@@ -1,14 +1,1 @@
-class Product{
-  public int product(int x,int y){
-      int re=x*y;
-      return re;
-    }
-    public int product(int x,int y,int z){
-      int re=x*y*z;
-      return re;
-    }
-    public double product(double x,double y){
-      double re=x*y;
-      return re;
-    }
-}
+
